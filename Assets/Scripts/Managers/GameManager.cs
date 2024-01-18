5e16d985bd428937fb7119c783a3ef643ade8db0
Lloyd <agdanlloyd@gmail.com>
2024-01-18 22:01:01 +0800

    initial commit

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    Player player;
+    public Slider healthBar;
+    bool isGamePaused = false;
+
+    public GameObject pauseGame;
+    public GameObject endingGame;
+    public GameObject losingGame;
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        healthBar.maxValue = player.maxPlayerHealth;
+    }
+   
+    void Update()
+    {
+        if (player.isDead)
+        {
+			losingGame.SetActive(true);
+			//Invoke("RestartGame", 0.4f);
+        }
+        UpdateUI();
+    }
+
+    public void RestartGame()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+        Time.timeScale = 1.0f;
+    }
+
+    void UpdateUI()
+    {
+        healthBar.value = player.currentPlayerHealth;
+        if (player.currentPlayerHealth <= 0)
+            healthBar.minValue = 0;
+    }
+
+    public void PauseGame()
+    {
+        isGamePaused = !isGamePaused;
+
+        if (isGamePaused == true)
+        {
+            Time.timeScale = 0.0f;
+            pauseGame.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+            pauseGame.SetActive(false);
+        }     
+    }
+
+    public void ExitGame()
+    {
+        SceneManager.LoadScene("Scenes/OpeningScene");
+        Time.timeScale = 1.0f;
+    }
+}
---
+++ Assets/Scripts/Player/Portal.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+
+    GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        if (collider.tag == "Player" && scene.name == "GameScene")
+        {
+            SceneManager.LoadScene("Scenes/GameScene-2");
+            Time.timeScale = 1.0f;
+        }
+        else if (collider.tag == "Player" && scene.name == "GameScene-2")
+        {
+			SceneManager.LoadScene("Scenes/GameScene-3");
+			//gameManager.endingGame.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+		else if (collider.tag == "Player" && scene.name == "GameScene-3")
+		{
+			gameManager.endingGame.SetActive(true);
+			Time.timeScale = 0.0f;
+		}
+	}
+}
